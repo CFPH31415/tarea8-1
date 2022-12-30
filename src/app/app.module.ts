06787118d83e4c620a5e8b0dc36e46304bae03b4
CFPH31415 <121558966+CFPH31415@users.noreply.github.com>
2022-12-30 10:37:57 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NglfDirective } from './nglf.directive';
+import { MostrarsiDirective } from './mostrarsi.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NglfDirective
+    MostrarsiDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
